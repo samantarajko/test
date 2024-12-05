3f0b021429cdb5200c568f9e315c1ad8a02711f9
samantarajko <149066331+samantarajko@users.noreply.github.com>
2024-12-05 12:10:27 +0100

    test2

---
+++ BookShop/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Provjera ishoda 1 i 2</h1>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
